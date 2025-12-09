f208785cafb11a0a936c828a23e91c377c987950
NicolasDS83600 <nico836001@outlook.fr>
2025-12-09 09:22:15 +0100

    feat: add function files
    - 11-binary_tree_size.c
    - 13-binary_tree_nodes.c
    - 15-binary_tree_is_full.c
    - 17-binary_tree_sibling.c

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: Pointer to the root node of the tree to measure
+ *
+ * Return: The number of nodes in the tree.
+ *         If tree is NULL, returns 0.
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_nodes - Counts the nodes with at least one child
+*
+* @tree: Pointer to the root node of the tree to count the nodes
+*
+* Return: Number of nodes that have at least one child.
+*         If tree is NULL, the function returns 0.
+*/
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t count = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL || tree->right != NULL)
+		count = 1;
+
+	count += binary_tree_nodes(tree->left);
+	count += binary_tree_nodes(tree->right);
+
+	return (count);
+}
